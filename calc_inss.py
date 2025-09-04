07a420aa58a97695c27c06aa7211e76d79919f93
Cainã Cavalcante <cainacavalcante352@gmail.com>
2025-09-04 13:21:05 -0300

    Update calc_inss.py

--- calc_inss.py
+++ calc_inss.py
@@ -22,24 +22,30 @@ def calc_inss(salário, vinculação):
     if vinculação == "clt": #Se o cidadão trabalhar de carteira assinada, seu desconto INSS será calculado da seguinte forma:
 
         if salário <= 1518.00:
+            aliquota = 7.5
             inss = salário * 0.075
 
         elif salário > 1518.00 and salário <= 2793.88:
+            aliquota = 9
             inss = salário * 0.09 - 22.77
 
         elif salário > 2793.88 and salário <= 4190.83:
+            aliquota = 12
             inss = salário * 0.12 - 106.59
 
-        elif salário > 4190.83 and salário <= 8157.41:
+        elif salário > 4190.83 and salário <= 8157.25:
+            aliquota = 14
             inss = salário * 0.14 - 190.40
-
-        elif salário > 8157.41:  
-            inss = salário * 0.14 - 190.40 #Mesmo que o salário do cidadão seja maior que R$8.157,41, o teto de contribuição INSS já foi atingido.
+    
+        elif salário > 8157.41:
+            aliquota = 11.66  
+            inss = 951.62 #Mesmo que o salário do cidadão seja maior que R$8.157,41, o teto de contribuição INSS já foi atingido.
         
 
     elif vinculação == "autônomo": #Se o cidadão for autônomo, seu desconto INSS será calculado da seguinte forma: 
 
         if salário <= 8157.41:
+            aliquota = 20
             inss = salário * 0.20
 
         else:
@@ -48,18 +54,20 @@ def calc_inss(salário, vinculação):
 
     elif vinculação == "empresário": #Se o cidadão for empresário, seu desconto INSS será calculado da seguinte forma:
 
-        if salário <= 8157.41:
+        if salário <= 14831.64:
+            aliquota = 11
             inss = salário * 0.11
 
-        elif salário > 8157.41:
+        elif salário > 14831.64:
+            inss = 1631.48
 
-            if inss > 1631.48:
-                inss = 1631.48 #Mesmo que o salário do cidadão seja maior que R$8.157,41, o teto de contribuição INSS já foi atingido.
+            # if inss > 1631.48:
+            #     inss = 1631.48 #Mesmo que o salário do cidadão seja maior que R$8.157,41, o teto de contribuição INSS já foi atingido.
 
-            else:
-                inss = salário * 0.11 
+            # else:
+            #     inss = salário * 0.11 
                 
-    return round(inss, 2)
+    return round(inss, 2), aliquota
 
 # resultado = calc_inss(salário)
 # print(f"Você pagará R${resultado} de INSS!")
@@ -73,12 +81,14 @@ def calcular():
     data = request.get_json()
     salário = float(data['salario'])
     vinculação = data['vinculacao'].lower()
-    resultado = calc_inss(salário, vinculação)
+    resultado, aliquota = calc_inss(salário, vinculação)
+
 
     if resultado is not None:
-        return jsonify({"inss": resultado})
+        return jsonify({"inss": resultado, "aliquota": aliquota})
     else:
         return jsonify({"error": "Tipo de vinculação inválido!"}), 400
     
 if __name__ == "__main__":
     app.run(debug=True)
+    
