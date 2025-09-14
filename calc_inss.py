dcc8ab9fb835416a1670f084c498df99ae00531e
Cainã Cavalcante <cainacavalcante352@gmail.com>
2025-09-14 13:29:12 -0300

    Update calc_inss.py

--- calc_inss.py
+++ calc_inss.py
@@ -72,15 +72,23 @@ def calc_inss(salário, tipo_segurado, modalidade):
         
         elif modalidade == "baixa renda":
 
-            if salário >= 1518.00 and salário <= 8157.41:
+            if salário > 1518.00:
                 aliquota = 5
-                inss = salário * 0.05
+                inss = 75.90
             
             else:
                 aliquota = 5
                 inss = 75.90
 
+    elif tipo_segurado == "mei":
 
+        if salário > 1518.00:
+            aliquota = 5
+            inss = 75.90
+        
+        else:
+            aliquota = 5
+            inss =  75.90
     
 
     elif tipo_segurado == "empresário": 
@@ -94,18 +102,31 @@ def calc_inss(salário, tipo_segurado, modalidade):
             inss = 1631.48
 
     elif tipo_segurado == "segurado especial":
+        if modalidade == "facultativo":
 
-        if salário >= 1518.00 and salário <= 8157.41:
-            aliquota = 20
-            inss = salário * 0.20
+            if salário >= 1518.00 and salário <= 8157.41:
+                aliquota = 20
+                inss = salário * 0.20
         
-        elif salário > 8157.41:
-            aliquota = 20
-            inss = 1631.48
+            elif salário > 8157.41:
+                aliquota = 20
+                inss = 1631.48
             
-        else:
-            aliquota = 20
-            inss = 303.60
+            else:
+                aliquota = 20
+                inss = 303.60
+        
+        elif modalidade == "obrigatório":
+            
+            if salário >= 1518.00:
+                aliquota = 1.3
+                inss = salário * 0.013
+
+            else:
+                aliquota = 1.3
+                inss = 19.74
+
+            
                 
     return round(inss, 2), aliquota
 
